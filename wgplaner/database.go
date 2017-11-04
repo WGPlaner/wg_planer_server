cce4dfe041c29af70acb42700a88e128473b0cb5
Andre Meyering <info@andremeyering.de>
2017-11-04 15:36:31 +0100

    [Database] Add connection; get user
    * add database connection using XORM
    * getUser now checks if the user exists on firebase and server
    * #WGPS-13 fixed

--- wgplaner/database.go
+++ wgplaner/database.go
@@ -5,24 +5,65 @@ import (
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/core"
 	"github.com/go-xorm/xorm"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/wgplaner/wg_planer_server/gen/models"
 )
 
+var implementedDrivers = []string{
+	DRIVER_MYSQL,
+	DRIVER_SQLITE,
+}
 var ormEngine *xorm.Engine
 
-func CreateSqlConnection() {
-	var err error
-	dbConfig := GetAppConfig().Database
-	dataSource := fmt.Sprintf("%s:%s@/%s?charset=utf-8", dbConfig.Server, dbConfig.Password, dbConfig.DatabaseName)
+//func sync(engine *xorm.Engine) error {
+//	return engine.Sync(&SyncLoginInfo2{}, &SyncUser2{}, &models.User{}, &models.Group{})
+//}
 
-	if ormEngine, err = xorm.NewEngine("mysql", dataSource); err != nil {
-		log.Fatal("[Configuration] SQL Connection failed! ", err)
-	}
+var OrmEngine *xorm.Engine
+
+func isValidDriverName(driverName string) bool {
+	return StringInSlice(driverName, implementedDrivers)
 }
 
-func GetOrmEngine() *xorm.Engine {
-	if ormEngine == nil {
-		CreateSqlConnection()
+func CreateOrmEngine(dbConfig *databaseConfig) *xorm.Engine {
+	var err error
+	var engine *xorm.Engine
+
+	switch AppConfig.Database.Driver {
+	case DRIVER_MYSQL:
+		engine, err = getMysqlEngine(dbConfig)
+	case DRIVER_SQLITE:
+		engine, err = xorm.NewEngine("sqlite3", dbConfig.SqliteFile)
 	}
-	return ormEngine
+
+	if err != nil {
+		log.Fatal("[Configuration] SQL Connection failed! ", err)
+		return nil
+	}
+
+	if err = syncDatabaseTables(engine); err != nil {
+		log.Fatal("[SQL] Synchronization failed! ", err)
+		return nil
+	}
+
+	engine.ShowSQL(true)
+	engine.Logger().SetLevel(core.LOG_DEBUG)
+
+	return engine
+}
+
+func getMysqlEngine(config *databaseConfig) (*xorm.Engine, error) {
+	dataSource := fmt.Sprintf("%s:%s@/%s?charset=utf-8", config.MysqlServer,
+		config.MysqlPassword, config.MysqlDatabaseName)
+
+	return xorm.NewEngine("mysql", dataSource)
+}
+
+func syncDatabaseTables(engine *xorm.Engine) error {
+	return engine.Sync(
+		&models.User{},
+		&models.Group{},
+	)
 }
