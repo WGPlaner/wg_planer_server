58aee6e6b6574f8ab97091408c7f2a176ff4202e
Andre Meyering <info@andremeyering.de>
2017-11-08 17:12:59 +0100

    [Utils] Add structs and functions for errors

---
+++ wgplaner/errors_test.go
@@ -0,0 +1,43 @@
+package wgplaner
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestErrorList_Add(t *testing.T) {
+	errList := ErrorList{}
+	errList.Add("First error")
+	if len(errList.errors) != 1 {
+		t.Error("Should contain 1 error!")
+	}
+}
+
+func TestErrorList_AddError(t *testing.T) {
+	errList := ErrorList{}
+	errList.AddError(errors.New("First error"))
+	if len(errList.errors) != 1 {
+		t.Error("Should contain 1 error!")
+	}
+}
+
+func TestErrorList_AddList(t *testing.T) {
+	destList := ErrorList{}
+	srcList := ErrorList{
+		errors: []error{
+			errors.New("First"),
+			errors.New("Second"),
+		},
+	}
+
+	destList.AddList(&srcList)
+
+	if len(destList.errors) != 2 {
+		t.Error("Should contain 2 copied errors!")
+	}
+
+	if destList.errors[0] != srcList.errors[0] ||
+		destList.errors[1] != srcList.errors[1] {
+		t.Error("Elements should be equal!")
+	}
+}
