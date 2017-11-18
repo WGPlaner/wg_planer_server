6141085569a96dab74531723118af572b36c17e1
Andre Meyering <info@andremeyering.de>
2017-11-18 23:01:12 +0100

    [Main] Outsource controller initialization

--- cmd/wgplaner-api/wgplaner-api.go
+++ cmd/wgplaner-api/wgplaner-api.go
@@ -1,60 +1,20 @@
 package main
 
 import (
-	"errors"
-	"io"
 	"log"
 	"math/rand"
 	"time"
 
 	"github.com/wgplaner/wg_planer_server/gen/restapi"
 	"github.com/wgplaner/wg_planer_server/gen/restapi/operations"
-	"github.com/wgplaner/wg_planer_server/gen/restapi/operations/group"
-	"github.com/wgplaner/wg_planer_server/gen/restapi/operations/info"
-	"github.com/wgplaner/wg_planer_server/gen/restapi/operations/shoppinglist"
-	"github.com/wgplaner/wg_planer_server/gen/restapi/operations/user"
 	"github.com/wgplaner/wg_planer_server/wgplaner"
 	"github.com/wgplaner/wg_planer_server/wgplaner/controllers"
 
 	"github.com/go-openapi/loads"
-	"github.com/go-openapi/runtime"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 )
-
-func initializeControllers(api *operations.WgplanerAPI) {
-	// Producers
-	api.HTMLProducer = runtime.ProducerFunc(func(writer io.Writer, data interface{}) error {
-		if html, ok := data.(string); !ok {
-			return errors.New("Error in HTML Producer")
-		} else {
-			_, err := writer.Write([]byte(html))
-			return err
-		}
-	})
-
-	// Authentication
-	api.UserIDAuthAuth = controllers.UserIDAuth
-	api.FirebaseIDAuthAuth = controllers.FirebaseIDAuth
-
-	// Create API handlers
-	api.InfoGetLatestVersionHandler = info.GetLatestVersionHandlerFunc(controllers.GetVersionInfo)
-	api.GroupCreateGroupHandler = group.CreateGroupHandlerFunc(controllers.CreateGroup)
-	api.GroupGetGroupHandler = group.GetGroupHandlerFunc(controllers.GetGroup)
-	api.GroupCreateGroupCodeHandler = group.CreateGroupCodeHandlerFunc(controllers.CreateGroupCode)
-	api.GroupJoinGroupHandler = group.JoinGroupHandlerFunc(controllers.JoinGroup)
-	api.GroupJoinGroupHelpHandler = group.JoinGroupHelpHandlerFunc(controllers.JoinGroupHelp)
-	api.GroupLeaveGroupHandler = group.LeaveGroupHandlerFunc(controllers.LeaveGroup)
-	api.UserCreateUserHandler = user.CreateUserHandlerFunc(controllers.CreateUser)
-	api.UserGetUserHandler = user.GetUserHandlerFunc(controllers.GetUser)
-	api.UserGetUserImageHandler = user.GetUserImageHandlerFunc(controllers.GetUserImage)
-	api.UserUpdateUserHandler = user.UpdateUserHandlerFunc(controllers.UpdateUser)
-	api.UserUpdateUserImageHandler = user.UpdateUserImageHandlerFunc(controllers.UpdateUserImage)
-	api.ShoppinglistCreateListItemHandler = shoppinglist.CreateListItemHandlerFunc(controllers.CreateListItem)
-	api.ShoppinglistUpdateListItemHandler = shoppinglist.UpdateListItemHandlerFunc(controllers.UpdateListItem)
-	api.ShoppinglistGetListItemsHandler = shoppinglist.GetListItemsHandlerFunc(controllers.GetListItems)
-}
 
 func main() {
 	var errSpec error
@@ -74,7 +34,7 @@ func main() {
 	wgplaner.AppConfig = wgplaner.LoadAppConfigOrFail()
 	wgplaner.OrmEngine = wgplaner.CreateOrmEngine(&wgplaner.AppConfig.Database)
 	wgplaner.FireBaseApp = wgplaner.CreateFirebaseConnection()
-	initializeControllers(api)
+	controllers.InitializeControllers(api)
 
 	if wgplaner.AppConfig.Mail.SendTestMail {
 		wgplaner.SendTestMail()
---
+++ wgplaner/controllers/controllers.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"errors"
+	"io"
+
+	"github.com/go-openapi/runtime"
+	"github.com/wgplaner/wg_planer_server/gen/restapi/operations"
+	"github.com/wgplaner/wg_planer_server/gen/restapi/operations/group"
+	"github.com/wgplaner/wg_planer_server/gen/restapi/operations/info"
+	"github.com/wgplaner/wg_planer_server/gen/restapi/operations/shoppinglist"
+	"github.com/wgplaner/wg_planer_server/gen/restapi/operations/user"
+)
+
+func InitializeControllers(api *operations.WgplanerAPI) {
+	// Producers
+	api.HTMLProducer = runtime.ProducerFunc(func(writer io.Writer, data interface{}) error {
+		if html, ok := data.(string); !ok {
+			return errors.New("error in HTML producer")
+		} else {
+			_, err := writer.Write([]byte(html))
+			return err
+		}
+	})
+
+	// Authentication
+	api.UserIDAuthAuth = UserIDAuth
+	api.FirebaseIDAuthAuth = FirebaseIDAuth
+
+	// Create API handlers
+	api.InfoGetLatestVersionHandler = info.GetLatestVersionHandlerFunc(GetVersionInfo)
+	api.GroupCreateGroupHandler = group.CreateGroupHandlerFunc(CreateGroup)
+	api.GroupGetGroupHandler = group.GetGroupHandlerFunc(GetGroup)
+	api.GroupCreateGroupCodeHandler = group.CreateGroupCodeHandlerFunc(CreateGroupCode)
+	api.GroupJoinGroupHandler = group.JoinGroupHandlerFunc(JoinGroup)
+	api.GroupJoinGroupHelpHandler = group.JoinGroupHelpHandlerFunc(JoinGroupHelp)
+	api.GroupLeaveGroupHandler = group.LeaveGroupHandlerFunc(LeaveGroup)
+	api.UserCreateUserHandler = user.CreateUserHandlerFunc(CreateUser)
+	api.UserGetUserHandler = user.GetUserHandlerFunc(GetUser)
+	api.UserGetUserImageHandler = user.GetUserImageHandlerFunc(GetUserImage)
+	api.UserUpdateUserHandler = user.UpdateUserHandlerFunc(UpdateUser)
+	api.UserUpdateUserImageHandler = user.UpdateUserImageHandlerFunc(UpdateUserImage)
+	api.ShoppinglistCreateListItemHandler = shoppinglist.CreateListItemHandlerFunc(CreateListItem)
+	api.ShoppinglistUpdateListItemHandler = shoppinglist.UpdateListItemHandlerFunc(UpdateListItem)
+	api.ShoppinglistGetListItemsHandler = shoppinglist.GetListItemsHandlerFunc(GetListItems)
+}
