75279ec91d456d33a61ffd922a7e9e7d23711d0b
Andre Meyering <info@andremeyering.de>
2017-11-20 20:57:39 +0100

    [Tests] Add user creation integration tests

--- integrations/user_test.go
+++ integrations/user_test.go
@@ -4,8 +4,10 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/go-openapi/swag"
 	"github.com/stretchr/testify/assert"
 	"github.com/wgplaner/wg_planer_server/gen/models"
+	"github.com/wgplaner/wg_planer_server/gen/restapi/operations/user"
 )
 
 func TestGetUserUnauthorized(t *testing.T) {
@@ -43,4 +45,76 @@ func TestGetOtherUser(t *testing.T) {
 		assert.Equal(t, userId, *apiUser.UID)
 		assert.Equal(t, "Max Meier", *apiUser.DisplayName)
 	}
+}
+
+func TestGetUserImage(t *testing.T) {
+	prepareTestEnv(t)
+	var (
+		authId      = "1234567890fakefirebaseid0001"
+		userId      = "1234567890fakefirebaseid0002"
+		urlBuilder  = user.GetUserImageURL{UserID: userId}
+		imgURL, err = urlBuilder.Build()
+	)
+
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	var (
+		req  = NewRequest(t, "GET", authId, imgURL.String())
+		resp = MakeRequest(t, req, http.StatusOK)
+	)
+
+	assert.Equal(t, resp.Headers.Get("Content-Type"), "application/octet-stream")
+}
+
+func TestCreateUserUnauthorized(t *testing.T) {
+	prepareTestEnv(t)
+	var (
+		errResp = models.ErrorResponse{}
+		newUser = models.User{
+			UID:         swag.String("1234567890fakefirebaseid0003"),
+			DisplayName: swag.String("Andre"),
+		}
+		req  = NewRequestWithJSON(t, "POST", "1234567890fakefirebaseid0001", "/users", newUser)
+		resp = MakeRequest(t, req, http.StatusUnauthorized)
+	)
+
+	if DecodeJSON(t, resp, &errResp) {
+		assert.NotEmpty(t, *errResp.Message)
+	}
+}
+
+func TestCreateUserMissingRequired(t *testing.T) {
+	prepareTestEnv(t)
+	var (
+		errResp = models.ErrorResponse{}
+		req     = NewRequestWithJSON(t, "POST", AuthValid, "/users", models.User{})
+		resp    = MakeRequest(t, req, http.StatusUnprocessableEntity)
+	)
+
+	if DecodeJSON(t, resp, &errResp) {
+		assert.NotEmpty(t, *errResp.Message)
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	prepareTestEnv(t)
+	var (
+		uid     = "1234567890fakefirebaseid0003"
+		newUser = models.User{
+			UID:         &uid,
+			DisplayName: swag.String("Andre"),
+		}
+		createdUser = models.User{}
+		req         = NewRequestWithJSON(t, "POST", uid, "/users", newUser)
+		resp        = MakeRequest(t, req, http.StatusOK)
+	)
+
+	if DecodeJSON(t, resp, &createdUser) {
+		assert.Equal(t, *createdUser.UID, *newUser.UID)
+		assert.Equal(t, *createdUser.DisplayName, *newUser.DisplayName)
+	}
+
+	// TODO: Load Beans
 }
