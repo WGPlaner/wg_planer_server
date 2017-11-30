081d7f35005366949bc27f7d64b8d22695bace46
Andre Meyering <info@andremeyering.de>
2017-11-30 18:07:31 +0100

    [Tests] Update coverage; distinguish between unit and integration tests

--- coverage.sh
+++ coverage.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env bash
 
-PACKAGES=$(go list ./... | grep -v -e vendor -e restapi)
-
-go get -u github.com/wadey/gocovmerge
+# Packages to test
+PACKAGES=$(go list ./... | grep -v -e vendor -e restapi -e integrations)
 
 # Unit tests
-for PKG in $PACKAGES; do
-	go test -cover -coverprofile $GOPATH/src/$PKG/coverage.out $PKG
-done;
+echo "mode: count" > unit.coverage.out
+for PKG in ${PACKAGES}; do
+	echo "" > package.coverage.out
+	go test -covermode count -coverprofile package.coverage.out ${PKG}
+	grep -h -v -e "^mode:" -e '^$' package.coverage.out >> unit.coverage.out
+done
 
 # Integration tests
-go test -c github.com/wgplaner/wg_planer_server/integrations -coverpkg $(echo $PACKAGES | tr ' ' ',') -o integrations.cover.test
+if [[ -z "${WGPLANER_ROOT}" ]]; then
+  echo "Environment variable WGPLANER_ROOT must be set for integration tests!"
+  exit 1
+fi
+
+go test -c github.com/wgplaner/wg_planer_server/integrations -covermode count -coverpkg $(echo ${PACKAGES} | tr ' ' ',') -o integrations.cover.test
 ./integrations.cover.test -test.coverprofile=integration.coverage.out
-
-echo "mode: set" > coverage.all
-for PKG in $PACKAGES; do
-	egrep "$PKG[^/]*\.go" integration.coverage.out
-	egrep "$PKG/[^/]*\.go" integration.coverage.out > int.coverage.out;
-
-	# TODO
-	#gocovmerge $GOPATH/src/$PKG/coverage.out int.coverage.out > pkg.coverage.out;
-	cat $GOPATH/src/$PKG/coverage.out int.coverage.out > pkg.coverage.out
-
-	grep -h -v "^mode:" pkg.coverage.out >> coverage.all;
-	mv pkg.coverage.out $GOPATH/src/$PKG/coverage.out;
-	rm int.coverage.out;
-done;
