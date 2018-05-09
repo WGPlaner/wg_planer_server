b8553c81b97e2e590df8f1307f20f64e4c2ded44
Andre Meyering <info@andremeyering.de>
2018-05-09 18:42:17 +0200

    Fix bintray deployment

--- scripts/bintray_deploy.sh
+++ scripts/bintray_deploy.sh
@@ -3,6 +3,9 @@
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 PROJECT_DIR=${SCRIPT_DIR}/..
 
+GIT_HASH=$(git --git-dir=".git" show --no-patch --pretty="%h")
+echo "GIT_HASH = ${GIT_HASH}"
+
 GIT_DATE=$(git --git-dir=".git" show --no-patch --pretty="%ci")
 echo "GIT_DATE = ${GIT_DATE}"
 
@@ -12,13 +15,21 @@ echo "RELEASE_DATE = ${RELEASE_DATE}"
 DATE_HASH=$(date -u +"%Y-%m-%d_%H-%M")
 echo "DATE_HASH = ${DATE_HASH}"
 
-cp wg_planer_server.tar.gz wg_planer_server_${DATE_HASH}.tar.gz
+VERSION_NAME="${TRAVIS_GO_VERSION}_${DATE_HASH}_git-${GIT_HASH}"
+echo "VERSION_NAME = ${VERSION_NAME}"
+
+FILENAME="wg_planer_${VERSION_NAME}.tar.gz"
+
+cd ${PROJECT_DIR}
+tar -zcvf wg_planer_server.tar.gz build
+cp wg_planer_server.tar.gz ${FILENAME}
+cd ${SCRIPT_DIR}
 
 cat > "${SCRIPT_DIR}/bintray.json" <<EOF
 {
 	"package": {
-		"name": "wg_planer_server",
-		"repo": "wg_planer_api",
+		"name": "wg_planer_api",
+		"repo": "wg_planer_server",
 		"subject": "bugwelle",
 		"website_url": "https://github.com/wgplaner/wg_planer_server",
 		"vcs_url": "https://github.com/wgplaner/wg_planer_server.git",
@@ -32,10 +43,13 @@ cat > "${SCRIPT_DIR}/bintray.json" <<EOF
 	"files":
 	[
 		{
-			"includePattern": "${PROJECT_DIR}/wg_planer_server_${DATE_HASH}.tar.gz",
-			"uploadPattern": "wg_planer_server_${DATE_HASH}.tar.gz"
+			"includePattern": "./${FILENAME}",
+			"uploadPattern": "${FILENAME}"
 		}
 	],
 	"publish": true
 }
 EOF
+
+echo "Config file:"
+cat ${SCRIPT_DIR}/bintray.json
